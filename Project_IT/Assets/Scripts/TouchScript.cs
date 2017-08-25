d09be8a25f1ad4df9e405c246e078bf7f29a15bd
annakoskina <30621269+annakoskina@users.noreply.github.com>
2017-08-25 13:23:09 +0300

    Project

---
+++ Project_IT/Assets/Scripts/TouchScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour
+{
+    public ParticleSystem blood;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           RaycastHit hit;
+
+           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Instantiate(blood, hit.point,Quaternion.identity);
+                
+            }
+        }
+    }
+}
---
+++ Project_IT/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,97 @@
+/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A custom handler that implements the ITrackableEventHandler interface.
+    /// </summary>
+    public class DefaultTrackableEventHandler : MonoBehaviour,
+                                                ITrackableEventHandler
+    {
+        public GUIStyle style;
+
+        private TrackableBehaviour mTrackableBehaviour;
+        private bool mShowGUIText = true;
+    
+        void Start()
+        {
+            mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+            if (mTrackableBehaviour)
+            {
+                mTrackableBehaviour.RegisterTrackableEventHandler(this);
+            }
+        }
+
+        public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+        {
+            if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+            {
+                mShowGUIText = false;
+                OnTrackingFound();
+            }
+            else
+            {
+                mShowGUIText = true;
+                OnTrackingLost();
+            }
+        }
+
+        private void OnTrackingFound()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Enable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = true;
+            }
+
+            // Enable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = true;
+            }
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+        }
+
+
+        private void OnTrackingLost()
+        {
+            Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+            Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+            // Disable rendering:
+            foreach (Renderer component in rendererComponents)
+            {
+                component.enabled = false;
+            }
+
+            // Disable colliders:
+            foreach (Collider component in colliderComponents)
+            {
+                component.enabled = false;
+            }
+
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+        }
+        void OnGUI()
+        {
+            if (mShowGUIText)
+            {
+                GUI.TextArea(new Rect(0, 0, 600, 60), "Please put mobile camera on a marker", style);
+            }
+        }
+    }
+}
